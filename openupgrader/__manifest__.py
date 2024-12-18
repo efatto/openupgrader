f052785fffcef82f8f12afe16a5859e429820cf1
sergiocorato <sergiocorato@gmail.com>
2024-12-18 18:31:22 +0100

    [IMP] create v. 13.0 branch

--- openupgrader/__manifest__.py
+++ openupgrader/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "OpenUpgrader",
-    "version": "14.0.1.0.5",
+    "version": "13.0.1.0.1",
     "category": "Odoo Management",
     "author": "Sergio Corato",
     "license": "AGPL-3",
