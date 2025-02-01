a0905430049574d81763922e01c9ec12d63acb0c
sergiocorato <sergiocorato@gmail.com>
2025-02-01 17:10:52 +0100

    [IMP] split method create venv

--- openupgrader/models/odoo_version.py
+++ openupgrader/models/odoo_version.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+
 from odoo import api, fields, models,  _
 from odoo.exceptions import UserError
 
@@ -45,9 +48,142 @@ class OdooVersion(models.Model):
 
     def button_create_venv(self):
         self.ensure_one()
-        openupgrader_migration = self.env["openupgrader.migration"].search([])
-        if len(openupgrader_migration) != 1:
-            raise UserError(_("Missing Openupgrader Migration record!"))
-        if len(openupgrader_migration) > 1:
-            raise UserError(_("Only one Openupgrader Migration record can be created!"))
-        openupgrader_migration.create_venv_git_version(self.name)
+        openupgrader_migration_id = self.env["openupgrader.migration"].search([])
+        # if len(openupgrader_migration) != 1:
+        #     raise UserError(_("Missing Openupgrader Migration record!"))
+        # if len(openupgrader_migration) > 1:
+        #     raise UserError(_("Only one Openupgrader Migration record can be created!"))
+        self.create_venv_git_version(self.name, openupgrader_migration_id)
+
+    def _get_env_for_subprocess(self, folder, py_version):
+        env_for_subprocess = os.environ.copy()
+        env_for_subprocess["VIRTUAL_ENV"] = folder
+        env_for_subprocess["PYTHONPATH"] = folder
+        env_for_subprocess["PATH"] = ":".join([
+            folder,
+            os.path.join(folder, "bin"),
+            "/bin/pip",
+        ])
+        env_for_subprocess["PWD"] = folder
+        python_root = os.path.join(
+            folder,
+            "lib",
+            f"python{'.'.join(py_version.split('.')[:2])}")
+        if os.path.isdir(python_root):
+            env_for_subprocess["LIBRARY_ROOTS"] = python_root
+        return env_for_subprocess
+
+    def _create_python_venv(self, venv_path, py_version):
+        # create virtualenv
+        subprocess_env = self._get_env_for_subprocess(venv_path, py_version)
+        if not os.path.isdir(venv_path):
+            subprocess.Popen([f'mkdir -p {venv_path}'], shell=True).wait()
+            # do not recreate virtualenv as it regenerate file with bug in split()
+            # ../openupgrade10.0/lib/python2.7/site-packages/pip/_internal/vcs/git.py
+        subprocess.Popen([f'pyenv install -s {py_version}'], shell=True).wait()
+        subprocess.Popen(
+            [f'pip install virtualenv'], shell=True).wait()
+        subprocess.Popen([f'ln -s /usr/bin/git'], cwd=venv_path, shell=True).wait()
+        subprocess.Popen(
+            [f'''virtualenv -p {
+                os.path.join(
+                    os.path.expanduser("~"),
+                    ".pyenv",
+                    "versions",
+                    py_version,
+                    "bin",
+                    "python"
+                )} {venv_path}'''],
+            cwd=venv_path,
+            shell=True).wait()
+        return subprocess_env
+
+    def create_venv_git_version(self, version_name, openupgrader_migration_id):
+        openupgrader_repo_obj = self.env["openupgrader.repo"]
+        version_repos = openupgrader_repo_obj.search([
+            ("odoo_version_id", "=", version_name),
+        ])
+        # if len(version_repos) != 1:
+        #     raise UserError(_("Version repositories not found!"))
+        if openupgrader_migration_id:
+            openupgrader_migration_id.write({
+                "repo_ids": [(6, 0, version_repos.ids)],
+            })
+        openupgrader_config_obj = self.env["openupgrader.config"]
+        config_ids = openupgrader_config_obj.search([
+            ('odoo_version_id', '=', version_name),
+        ])
+        # if len(config_ids) != 1:
+        #     raise UserError(_("OpenUpgrader config not found!"))
+        if openupgrader_migration_id:
+            openupgrader_migration_id.write({
+                "config_ids": [(6, 0, config_ids.ids)],
+            })
+        odoo_is_openupgrade = self.odoo_is_openupgrade
+        # Odoo is OpenUpgrade until v. 13.0, from v. 14.0 Odoo is in ./<version/odoo
+        # install odoo Openupgrade repo, from v. 14.0 it contains only migration script
+        if openupgrader_migration_id:
+            venv_path = os.path.join(
+                openupgrader_migration_id.folder, f"openupgrade{version_name}")
+            subprocess_env = self._create_python_venv(venv_path, self.python_version)
+            openupgrade_path = os.path.join(venv_path, "odoo")
+            odoo_path = openupgrade_path if odoo_is_openupgrade else (
+                os.path.join(venv_path, "repos", 'odoo'))
+            if not os.path.isdir(openupgrade_path):
+                subprocess.Popen([
+                    f"git clone --single-branch "
+                    f"{openupgrader_migration_id.openupgrade_repo} -b {version_name} "
+                    "--depth 1 odoo "
+                ],
+                    cwd=venv_path,
+                    env=subprocess_env,  # forse qui non serve
+                    shell=True
+                ).wait()
+            else:
+                subprocess.Popen([
+                    f'git reset --hard origin/{version_name}',
+                    'git pull',
+                    f'git reset --hard origin/{version_name}',
+                ],
+                    cwd=openupgrade_path,
+                    env=subprocess_env,  # forse qui non serve
+                    shell=True,
+                ).wait()
+
+            if not odoo_is_openupgrade:
+                # install odoo repo
+                openupgrader_migration_id.install_repo(
+                    "odoo", openupgrader_migration_id.odoo_repo,
+                    version_name, version_name,
+                    os.path.join(venv_path, "repos", "odoo"))
+            commands = [
+                'bin/pip install "setuptools<58.0.0"',
+                'bin/pip install -r '
+                f'{openupgrade_path if odoo_is_openupgrade else odoo_path}'
+                '/requirements.txt',
+                f'cd {openupgrade_path} && ../bin/pip install -e . '
+                if odoo_is_openupgrade
+                else f'cd {odoo_path} && ../../bin/pip install -e . ',
+            ]
+            for command in commands:
+                subprocess.Popen(
+                    command,
+                    cwd=venv_path,
+                    env=subprocess_env,
+                    shell=True,
+                ).wait()
+            extra_paths = ['%s/addons-extra' % venv_path, '%s/repos' % venv_path]
+            for path in extra_paths:
+                if not os.path.isdir(path):
+                    process = subprocess.Popen('mkdir %s' % path, cwd=venv_path,
+                                               shell=True)
+                    process.wait()
+            migration_log_path = os.path.join(venv_path, 'migration.log')
+            if os.path.isfile(migration_log_path):
+                os.remove(migration_log_path)
+
+            for remote_repo in version_repos.remote_repo_ids:
+                openupgrader_migration_id.install_repo(
+                    remote_repo.name, remote_repo.remote_url,
+                    remote_repo.remote_branch, version_name, venv_path)
+            openupgrader_migration_id.state = 'created_venv'
--- openupgrader/models/openupgrader_migration.py
+++ openupgrader/models/openupgrader_migration.py
@@ -558,7 +558,9 @@ class OpenupgraderMigration(models.Model):
             f'origin/{repo_version}'
         ], shell=True)
         process.wait()
-        # copy modules to create a unique addons path
+        # copy modules to create a unique addons path, unless it's odoo repo
+        if repo_name == "odoo":
+            return
         for root, dirs, files in os.walk(repo_path):
             for d in dirs:
                 if d not in ['.git', 'setup']:
@@ -568,134 +570,6 @@ class OpenupgraderMigration(models.Model):
                     ], shell=True)
                     process.wait()
             break
-
-    def _get_env_for_subprocess(self, folder, py_version):
-        env_for_subprocess = os.environ.copy()
-        env_for_subprocess["VIRTUAL_ENV"] = folder
-        env_for_subprocess["PYTHONPATH"] = folder
-        env_for_subprocess["PATH"] = ":".join([
-            folder,
-            os.path.join(folder, "bin"),
-        ])
-        env_for_subprocess["PWD"] = folder
-        python_root = os.path.join(
-            folder,
-            "lib",
-            f"python{'.'.join(py_version.split('.')[:2])}")
-        if os.path.isdir(python_root):
-            env_for_subprocess["LIBRARY_ROOTS"] = python_root
-        return env_for_subprocess
-
-    def _create_python_venv(self, venv_path, py_version):
-        # create virtualenv
-        subprocess_env = self._get_env_for_subprocess(venv_path, py_version)
-        if not os.path.isdir(venv_path):
-            subprocess.Popen([f'mkdir -p {venv_path}'], shell=True).wait()
-            # do not recreate virtualenv as it regenerate file with bug in split()
-            # ../openupgrade10.0/lib/python2.7/site-packages/pip/_internal/vcs/git.py
-        subprocess.Popen([f'pyenv install -s {py_version}'], shell=True).wait()
-        subprocess.Popen(
-            [f'bin/pip install virtualenv'], cwd=venv_path, shell=True).wait()
-        subprocess.Popen([f'ln -s /usr/bin/git'], cwd=venv_path, shell=True).wait()
-        subprocess.Popen(
-            [f'''virtualenv -p {
-                os.path.join(
-                    os.path.expanduser("~"),
-                    ".pyenv",
-                    "versions",
-                    py_version,
-                    "bin",
-                    "python"
-                )} {venv_path}'''],
-            cwd=venv_path,
-            env=subprocess_env,
-            shell=True).wait()
-        return subprocess_env
-
-    def create_venv_git_version(self, version_name):
-        openupgrader_repo_obj = self.env["openupgrader.repo"]
-        version_repos = openupgrader_repo_obj.search([
-            ("odoo_version_id", "=", version_name),
-        ])
-        if len(version_repos) != 1:
-            raise UserError(_("Version repositories not found!"))
-        self.write({
-            "repo_ids": [(6, 0, version_repos.ids)],
-        })
-        openupgrader_config_obj = self.env["openupgrader.config"]
-        config_ids = openupgrader_config_obj.search([
-            ('odoo_version_id', '=', version_name),
-        ])
-        if len(config_ids) != 1:
-            raise UserError(_("OpenUpgrader config not found!"))
-        self.write({
-            "config_ids": [(6, 0, config_ids.ids)],
-        })
-        odoo_is_openupgrade = config_ids.odoo_version_id.odoo_is_openupgrade
-        # Install OpenUpgrade repository always in ./<folder>/<version>/odoo/
-        # Odoo is OpenUpgrade until v. 13.0, from v. 14.0 Odoo is in ./<version/odoo
-
-        venv_path = os.path.join(self.folder, f"openupgrade{version_name}")
-        openupgrade_path = os.path.join(venv_path, "odoo")
-        odoo_path = openupgrade_path if odoo_is_openupgrade else (
-            os.path.join(openupgrade_path, "repos", 'odoo'))
-        py_version = config_ids.odoo_version_id.python_version
-        subprocess_env = self._create_python_venv(venv_path, py_version)
-        # install odoo Openupgrade repo, from v. 14.0 it contains only migration script
-        if not os.path.isdir(openupgrade_path):
-            subprocess.Popen([
-                'cd %s && git clone --single-branch %s -b %s --depth 1 odoo' % (
-                    venv_path, self.openupgrade_repo, version_name)
-            ],
-                cwd=venv_path,
-                env=subprocess_env,  # forse qui non serve
-                shell=True
-            ).wait()
-        else:
-            subprocess.Popen([
-                'cd %s && git reset --hard origin/%s && git pull '
-                '&& git reset --hard origin/%s' % (
-                    openupgrade_path, version_name, version_name
-                )
-            ],
-                cwd=venv_path,
-                env=subprocess_env,  # forse qui non serve
-                shell=True,
-            ).wait()
-
-        if not odoo_is_openupgrade:
-            # install odoo repo
-            self.install_repo( # TODO THIS DO NOT WORK?? install requirements for odoo from repos!!!
-                "odoo", self.odoo_repo, version_name, version_name,
-                os.path.join(venv_path, "repos", "odoo"))
-        commands = [
-            'bin/pip install "setuptools<58.0.0"',
-            'bin/pip install -r '
-            f'{openupgrade_path if odoo_is_openupgrade else odoo_path}'
-            '/requirements.txt',
-            f'cd {openupgrade_path} && ../bin/pip install -e . ' if odoo_is_openupgrade
-            else f'cd {odoo_path} && ../../bin/pip install -e . ',
-        ]
-        for command in commands:
-            subprocess.Popen(
-                command,
-                cwd=venv_path,
-                env=subprocess_env,
-                shell=True,
-            ).wait()
-        extra_paths = ['%s/addons-extra' % venv_path, '%s/repos' % venv_path]
-        for path in extra_paths:
-            if not os.path.isdir(path):
-                process = subprocess.Popen('mkdir %s' % path, cwd=venv_path, shell=True)
-                process.wait()
-        migration_log_path = os.path.join(venv_path, 'migration.log')
-        if os.path.isfile(migration_log_path):
-            os.remove(migration_log_path)
-
-        for remote_repo in version_repos.remote_repo_ids:
-            self.install_repo(remote_repo.name, remote_repo.remote_url,
-                              remote_repo.remote_branch, version_name, venv_path)
-        self.state = 'created_venv'
 
     def auto_install_modules(self, version):
         self.start_odoo(version)
