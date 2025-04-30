a6f1e0091d55ea27a9e68e38fb80240217a95047
sergiocorato <sergiocorato@gmail.com>
2025-04-30 14:36:18 +0200

    [FIX] create configuration for every version

--- openupgrader/tests/test_openupgrader.py
+++ openupgrader/tests/test_openupgrader.py
@@ -31,29 +31,33 @@ class Openupgrader(SavepointCase):
         cls.to_version_id = cls.version_obj.search([
             ("name", "=", cls.to_version),
         ])
-        cls.openupgrader_config = cls.env["openupgrader.config"].search([
-            ("odoo_version_id.name", "=", cls.from_version),
-        ])
-        if not cls.openupgrader_config:
-            cls.openupgrader_config = cls.env["openupgrader.config"].create({
-                "odoo_version_id": cls.from_version_id.id,
-            })
-        config_file_path = get_module_resource(
-            "openupgrader", "tests", "data", "openupgrader_config.yml"
-        )
-        with open(config_file_path, "rb") as file:
-            config_file = base64.b64encode(file.read())
-            file.close()
-            cls.openupgrader_config.config_file = config_file
-            cls.openupgrader_config.button_load_config()
-        repos_file_path = get_module_resource(
-            "openupgrader", "tests", "data", "openupgrader_repos.yml"
-        )
-        with open(repos_file_path, "rb") as file:
-            repos_file = base64.b64encode(file.read())
-            file.close()
-            cls.openupgrader_config.repos_file = repos_file
-            cls.openupgrader_config.button_load_repos()
+        for version in [cls.from_version, cls.middle_version, cls.to_version]:
+            openupgrader_config = cls.env["openupgrader.config"].search([
+                ("odoo_version_id.name", "=", version),
+            ])
+            if not openupgrader_config:
+                version_id = cls.version_obj.search([
+                    ("name", "=", version),
+                ])
+                openupgrader_config = cls.env["openupgrader.config"].create({
+                    "odoo_version_id": version_id.id,
+                })
+            config_file_path = get_module_resource(
+                "openupgrader", "tests", "data", "openupgrader_config.yml"
+            )
+            with open(config_file_path, "rb") as file:
+                config_file = base64.b64encode(file.read())
+                file.close()
+                openupgrader_config.config_file = config_file
+                openupgrader_config.button_load_config()
+            repos_file_path = get_module_resource(
+                "openupgrader", "tests", "data", "openupgrader_repos.yml"
+            )
+            with open(repos_file_path, "rb") as file:
+                repos_file = base64.b64encode(file.read())
+                file.close()
+                openupgrader_config.repos_file = repos_file
+                openupgrader_config.button_load_repos()
         cls.openupgrader_migration = cls.env["openupgrader.migration"].create({
             "from_version_id": cls.from_version_id.id,
             "to_version_id": cls.to_version_id.id,
@@ -81,3 +85,4 @@ class Openupgrader(SavepointCase):
             self.openupgrader_migration.state,
             "db_migrated"
         )
+        # check actual migrated version is the middle one
