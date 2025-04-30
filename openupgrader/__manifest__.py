259e035b0564da22be2a7d93b16bd60b6ac8e08d
sergiocorato <sergiocorato@gmail.com>
2025-04-30 19:26:18 +0200

    [IMP] version

--- openupgrader/__manifest__.py
+++ openupgrader/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Openupgrader",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Odoo Management",
     "author": "Sergio Corato",
     "license": "AGPL-3",
