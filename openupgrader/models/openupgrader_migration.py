bba6a3b83bf89ae45733e04c9952dd766cc9e6f9
sergiocorato <sergiocorato@gmail.com>
2025-06-14 11:21:00 +0200

    [WIP] log files

--- openupgrader/models/openupgrader_migration.py
+++ openupgrader/models/openupgrader_migration.py
@@ -503,6 +503,7 @@ class OpenupgraderMigration(models.Model):
                     f"{os.path.join(dump_dir, 'filestore')}")
                 path = shutil.copytree(filestore, os.path.join(dump_dir, "filestore"))
                 logger.info(f"Path of copied filestore {path}")
+                logger.info(f"ls of copied files {os.listdir(path)}")
             with open(os.path.join(dump_dir, "manifest.json"), "w") as fh:
                 db = db_connect(db_name)
                 with db.cursor() as cr:
