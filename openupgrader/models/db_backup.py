fe843181cdb25713e03c1a4425c139f49bd1932a
sergiocorato <sergiocorato@gmail.com>
2025-06-14 18:32:03 +0200

    [FIX] pre-commit

--- openupgrader/models/db_backup.py
+++ openupgrader/models/db_backup.py
@@ -1,21 +1,22 @@
 import json
+import logging
 import os
 import shutil
 import tempfile
-import logging
 from datetime import datetime
-from odoo.modules import get_module_resource
+
+from odoo import fields, models
 from odoo.sql_db import db_connect
-from odoo.tools import config, exec_pg_command
+from odoo.tools import exec_pg_command
 from odoo.tools.osutil import zip_dir
-from odoo import fields, models, _
+
 logger = logging.getLogger(__name__)
 
 
 class DbBackup(models.Model):
-    _inherit = 'db.backup'
+    _inherit = "db.backup"
 
-    is_migration_backup = fields.Boolean(string='Is Migration Backup?')
+    is_migration_backup = fields.Boolean(string="Is Migration Backup?")
     odoo_version_id = fields.Many2one(
         comodel_name="odoo.version",
         string="Odoo version",
@@ -41,7 +42,6 @@ class DbBackup(models.Model):
 
     def action_backup_migration(self):
         """Run selected backups."""
-        backup = None
         successful = self.browse()
 
         # Start with local storage
@@ -49,7 +49,8 @@ class DbBackup(models.Model):
         for rec in self.filtered(lambda r: r.method == "local"):
             filename = (
                 f"{self.env.cr.dbname}_migrate.{version_name}."
-                f"{self.filename(datetime.now(), ext=rec.backup_format)}")
+                f"{self.filename(datetime.now(), ext=rec.backup_format)}"
+            )
             with rec.backup_log():
                 # Directory must exist
                 try:
@@ -63,7 +64,6 @@ class DbBackup(models.Model):
                         stream=destiny,
                         backup_format=rec.backup_format,
                     )
-                    backup = destiny.name
                 successful |= rec
 
         # Ensure a local backup exists if we are going to write it remotely
@@ -72,7 +72,8 @@ class DbBackup(models.Model):
             for rec in sftp:
                 filename = (
                     f"{self.env.cr.dbname}_migrate.{version_name}."
-                    f"{self.filename(datetime.now(), ext=rec.backup_format)}")
+                    f"{self.filename(datetime.now(), ext=rec.backup_format)}"
+                )
                 with rec.backup_log():
                     cached = rec.dump_db_migration(
                         db_name=f"{self.env.cr.dbname}_migrate",
@@ -105,10 +106,11 @@ class DbBackup(models.Model):
         openupgrader_migration_id = self.env["openupgrader.migration"].search([])
         openupgrader_migration_id.ensure_one()
         filestore = openupgrader_migration_id.get_filestore_path(
-            version_name=version_name, migration_folder=True)
+            version_name=version_name, migration_folder=True
+        )
         with tempfile.TemporaryDirectory() as dump_dir:
             if os.path.exists(filestore):
-                path = shutil.copytree(filestore, os.path.join(dump_dir, "filestore"))
+                shutil.copytree(filestore, os.path.join(dump_dir, "filestore"))
             with open(os.path.join(dump_dir, "manifest.json"), "w") as fh:
                 db = db_connect(db_name)
                 with db.cursor() as cr:
