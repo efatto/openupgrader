f22c371b7e37af727a6b11a6267f28291387a68c
sergiocorato <sergiocorato@gmail.com>
2025-07-03 16:26:37 +0200

    [FIX] pre-commit

--- openupgrader_l10n_it_ddt_to_dn/models/openupgrader_migration.py
+++ openupgrader_l10n_it_ddt_to_dn/models/openupgrader_migration.py
@@ -6,8 +6,8 @@ class OpenupgraderMigration(models.Model):
 
     def button_prepare_for_migration(self):
         res = super().button_prepare_for_migration()
-        if self.from_version_id.name == '12.0':
-             self.migrate_l10n_it_ddt_to_l10n_it_delivery_note()
+        if self.from_version_id.name == "12.0":
+            self.migrate_l10n_it_ddt_to_l10n_it_delivery_note()
         return res
 
     def migrate_l10n_it_ddt_to_l10n_it_delivery_note(self):
@@ -15,15 +15,16 @@ class OpenupgraderMigration(models.Model):
         self.start_odoo(self.from_version_id)
         odoo_client = self.odoo_connect()
         module_obj = odoo_client.env["ir.module.module"]
-        if module_obj.search([
-            ('name', '=', 'l10n_it_ddt'),
-            ('state', '=', 'installed'),
-        ]):
-            self.install_uninstall_module(
-                'l10n_it_delivery_note', install=True
-            )
+        if module_obj.search(
+            [
+                ("name", "=", "l10n_it_ddt"),
+                ("state", "=", "installed"),
+            ]
+        ):
+            self.install_uninstall_module("l10n_it_delivery_note", install=True)
             self.button_stop_odoo()
             self.start_odoo(
-              version_id=self.from_version_id,
-              extra_command=f'migrate_l10n_it_ddt -d {self.env.cr.dbname}_migrate')
+                version_id=self.from_version_id,
+                extra_command=f"migrate_l10n_it_ddt -d {self.env.cr.dbname}_migrate",
+            )
         self.button_stop_odoo()
