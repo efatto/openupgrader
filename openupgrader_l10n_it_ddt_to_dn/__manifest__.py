1dfce0769c878043ff3241dcee1daabb74925293
sergiocorato <sergiocorato@gmail.com>
2025-08-13 14:00:39 +0200

    [IMP] bump version

--- openupgrader_l10n_it_ddt_to_dn/__manifest__.py
+++ openupgrader_l10n_it_ddt_to_dn/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "OpenUpgrader extra method to migrate l10n_it_ddt",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "category": "other",
     "author": "Sergio Corato",
     "license": "AGPL-3",
