f5f495fc480c62403cc8d3207cd59f89569647b9
sergiocorato <sergiocorato@gmail.com>
2025-12-09 09:21:09 +0100

    [IMP] bump version

--- openupgrader/__manifest__.py
+++ openupgrader/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "OpenUpgrader",
-    "version": "12.0.1.0.8",
+    "version": "12.0.1.0.9",
     "category": "Odoo Management",
     "author": "Sergio Corato",
     "license": "AGPL-3",
